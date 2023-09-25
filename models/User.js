0c35916a995d420f39df96bd3867c1d896546761
jannecook <135638400+jannecook@users.noreply.github.com>
2023-09-25 16:50:53 -0700

    Updated to include signup.html and signup.js file

--- models/User.js
+++ models/User.js
@@ -59,7 +59,10 @@ User.init(
         return newUserData;
       },
       beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
         return updatedUserData;
       },
     },
