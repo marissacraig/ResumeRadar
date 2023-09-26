3f95a6806a6c82b30772f551b9ef97f263518e36
desktopbuddy <hanchondo21@gmail.com>
2023-09-25 22:44:52 -0700

    Fix application/job model associations

--- models/index.js
+++ models/index.js
@@ -2,20 +2,23 @@ const User = require('./User');
 const Job = require('./Job');
 const Application = require('./Application');
 
+// one user has many applications
 User.hasMany(Application, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE',
 });
-User.hasMany(Job, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
-Job.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'cascade',
-});
 Application.belongsTo(User, {
   foreignKey: 'user_id',
+});
+
+// one application has one job, one job may have many applications
+Application.hasOne(Job, {
+  foreignKey: 'user_id',
+  onDelete: 'RESTRICT',
+});
+Job.belongsToMany(Application, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
 });
 
 module.exports = { User, Application, Job };
