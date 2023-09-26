28609b9447954577eb7ad8c3e018d09e7b759e35
desktopbuddy <hanchondo21@gmail.com>
2023-09-25 22:41:22 -0700

    set up GET dashboard route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,6 +27,32 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Load dashboard displaying all of logged in user's applications
+router.get('/dashboard', async (req, res) => {
+  try {
+    const applicationData = await Application.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      include: [
+        {
+          model: Job,
+        }
+      ]
+    });
+
+    const applications = applicationData.map((application) => application.get({ plain: true }));
+
+    res.render('dashboard', {
+      applications,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 router.get('/project/:id', async (req, res) => {
   try {
     const projectData = await Project.findByPk(req.params.id, {
