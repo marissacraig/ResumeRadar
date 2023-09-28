0c385490017535f0d245eb949068e19edc56f96f
marissacraig <gersen.marissa@gmail.com>
2023-09-28 11:55:39 -0700

    created new js file and added starter code

---
+++ public/js/applicationDetails.js
@@ -0,0 +1,63 @@
+const jobTitleElement = document.getElementById('otherJobTitle');
+jobTitleElement.visibility = 'hidden';
+const companyElement = document.getElementById('otherCompany');
+companyElement.visibility = 'hidden';
+const statusElement = document.getElementById('otherStatus');
+statusElement.visibility = 'hidden';
+
+function otherJobTitle(val) {
+  if (val === 'Other') {
+    jobTitleElement.style.visibility = 'visible',
+    jobTitleElement.value = '',
+  } else {
+    jobTitleElement.style.visibility = 'hidden'
+  }
+};
+
+function otherCompany(val) {
+    if (val === 'Other') {
+      companyElement.style.visibility = 'visible',
+      companyElement.value = '',
+    } else {
+      companyElement.style.visibility = 'hidden'
+    }
+  };
+
+  function otherStatus(val) {
+    if (val === 'Other') {
+      statusElement.style.visibility = 'visible',
+      statusElement.value = '',
+    } else {
+      statusElement.style.visibility = 'hidden'
+    }
+  };
+
+async function newApplicationFormHandler(event) {
+    event.preventDefault();
+
+    const jobTitle = document.getElementById('#jobTitle').value.trim();
+    const jobCompany = document.getElementById('#company').value.trim();
+    const status = document.getElementById('#status').value.trim();
+    const location = document.getElementById('#location').value.trim();
+    const url = document.getElementById('#companyURL').value.trim();
+    const description = document.getElementById('#description').value.trim();
+    const salary = document.getElementById('#salary').value.trim();
+
+
+    if (jobTitle && jobCompany && status && location && url && description && salary) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ jobTitle, jobCompany, status, location, url, description, salary }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+
+document.addEventListener('submit', newApplicationFormHandler());
