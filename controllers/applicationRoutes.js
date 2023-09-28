ec5a50a8213ae4a373d53918f7b5a3e07acec63b
desktopbuddy <hanchondo21@gmail.com>
2023-09-27 20:21:40 -0700

    route application details, update models to work on applicationDetails.handlebars

--- controllers/applicationRoutes.js
+++ controllers/applicationRoutes.js
@@ -3,6 +3,32 @@ const { Application, User, Job } = require('../models');
 const withAuth = require('../utils/auth');
 
 // GET application details page by id
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const applicationData = await Application.findByPk(req.params.id, {
+      include: [
+        {
+          model: Job,
+        }
+      ]
+    });
+
+    if (!applicationData) {
+      res.status(404).json({ message: 'No application found with this id!' });
+      return;
+    }
+
+    const application = applicationData.get({ plain: true });
+
+    res.render('applicationDetails', {
+      application, 
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // GET edit application by id
 
