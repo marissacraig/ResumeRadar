36239df3b39d61316b6b4f318a81f9eefd3cc87d
marissacraig <gersen.marissa@gmail.com>
2023-10-01 13:56:14 -0700

    updated signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,31 +1,30 @@
-const btn = document.querySelector('.btn');
-btn.disabled = true;
-
-const verifyPassword = function() {
-    const confirmPasswordValue = document.querySelector('#confirm-password-signup').value;
-    const passwordValue = document.querySelector('#password-signup').value;
-    const message = document.querySelector('#message');
-    if (passwordValue !== confirmPasswordValue || passwordValue === "") {
-        message.innerHTML = 'Password and Confirm Password must match'; 
-        message.style.color = 'red';
-        btn.disabled = true;
-      } else {
-        btn.disabled = false;
-        message.innerHTML = "Password and Confirm Password match";
-        message.style.color = 'green';
-      }
-    }; 
+function verifyPassword() {
+  const btn = document.querySelector('.btn');
+  const confirmPasswordValue = document.querySelector('#confirm-password-signup').value;
+  const passwordValue = document.querySelector('#password-signup').value;
+  const message = document.querySelector('#message');
+  if (passwordValue !== confirmPasswordValue || passwordValue === '') {
+    message.innerHTML = 'Password and Confirm Password must match';
+    message.style.color = 'red';
+    btn.disabled = true;
+  } else {
+    btn.disabled = false;
+    message.innerHTML = "Password and Confirm Password match";
+    message.style.color = 'green';
+  }
+} 
 
 const signupFormHandler = async (event) => {
     event.preventDefault();
-  
-    const username = document.querySelector('#name-signup').value.trim();
+
+    const name = document.querySelector('#name-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
     if (username && email && password) {
       const response = await fetch('/api/users/signup', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ name, username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
       const jsonedRes = await response.json();
@@ -35,7 +34,6 @@ const signupFormHandler = async (event) => {
       }
     }
   };
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+
+document.querySelector('.signup-form')
+document.addEventListener('submit', signupFormHandler);
