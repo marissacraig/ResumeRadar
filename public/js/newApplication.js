80f0ab09acc72c65a358e87e974415f7a7179629
desktopbuddy <hanchondo21@gmail.com>
2023-09-30 19:56:18 -0700

    get custom text input value if Other selected

--- public/js/newApplication.js
+++ public/js/newApplication.js
@@ -1,48 +1,24 @@
-const jobTitleElement = document.getElementById('otherJobTitle');
-jobTitleElement.visibility = 'hidden';
-const companyElement = document.getElementById('otherCompany');
-companyElement.visibility = 'hidden';
-const statusElement = document.getElementById('otherStatus');
-statusElement.visibility = 'hidden';
-
-function otherJobTitle(val) {
-  if (val === 'Other') {
-    jobTitleElement.style.visibility = 'visible',
-    jobTitleElement.value = ''
-  } else {
-    jobTitleElement.style.visibility = 'hidden'
-  }
-};
-
-function otherCompany(val) {
-    if (val === 'Other') {
-      companyElement.style.visibility = 'visible',
-      companyElement.value = ''
-    } else {
-      companyElement.style.visibility = 'hidden'
-    }
-  };
-
-  function otherStatus(val) {
-    if (val === 'Other') {
-      statusElement.style.visibility = 'visible',
-      statusElement.value = ''
-    } else {
-      statusElement.style.visibility = 'hidden'
-    }
-  };
-
 async function newApplicationFormHandler(event) {
     event.preventDefault();
 
-    const title = document.getElementById('jobTitle').value.trim();
-    const company = document.getElementById('company').value.trim();
-    const status = document.getElementById('status').value.trim();
+    let title = document.getElementById('jobTitle').value.trim();
+    let company = document.getElementById('company').value.trim();
+    let status = document.getElementById('status').value.trim();
     const location = document.getElementById('location').value.trim();
     const url = document.getElementById('companyURL').value.trim();
     const description = document.getElementById('description').value.trim();
     const salary = document.getElementById('salary').value.trim();
 
+    // Get custom text input values for fields that have "other" option selected
+    if (title == "Other") {
+      title = document.getElementById('otherJobTitle').value.trim();;
+    }
+    if (company == "Other") {
+      company = document.getElementById('otherCompany').value.trim();
+    }
+    if (status == "Other") {
+      status = document.getElementById('otherStatus').value.trim();
+    }
 
     if (title && company && status && location && url && description && salary) {
         const response = await fetch('/api/applications', {
@@ -58,6 +34,5 @@ async function newApplicationFormHandler(event) {
         }
     }
 }
-
 
 document.querySelector('.new-application-form').addEventListener('submit', newApplicationFormHandler);
