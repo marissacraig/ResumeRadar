6d987a13e3e904b7125af13303d18bf076c374d2
desktopbuddy <hanchondo21@gmail.com>
2023-09-30 17:25:11 -0700

    add PUT method for updating application

--- controllers/api/applicationRoutes.js
+++ controllers/api/applicationRoutes.js
@@ -22,4 +22,33 @@ router.post('/', async (req, res) => {
   }
 });
 
+// UPDATE application
+router.put('/:id', async (req, res) => {
+  try {
+    const applicationData = await Application.update({
+      title: req.body.title,
+      company: req.body.company,
+      status: req.body.status,
+      location: req.body.location,
+      url: req.body.url,
+      description: req.body.description,
+      salary: req.body.salary,
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!applicationData) {
+      res.status(404).json({ message: 'No application found with this id' });
+      return;
+    }
+
+    res.status(200).json(applicationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
