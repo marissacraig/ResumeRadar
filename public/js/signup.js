205aa0eff37f8c6eb4638c0f1cb629a5e60164d6
julie <you@example.com>
2023-10-01 15:28:21 -0700

    fixed signup bugs

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,8 @@
 function verifyPassword() {
   const btn = document.querySelector('.btn');
-  const confirmPasswordValue = document.querySelector('#confirm-password-signup').value;
+  const confirmPasswordValue = document.querySelector(
+    '#confirm-password-signup'
+  ).value;
   const passwordValue = document.querySelector('#password-signup').value;
   const message = document.querySelector('#message');
   if (passwordValue !== confirmPasswordValue || passwordValue === '') {
@@ -9,31 +11,31 @@ function verifyPassword() {
     btn.disabled = true;
   } else {
     btn.disabled = false;
-    message.innerHTML = "Password and Confirm Password match";
+    message.innerHTML = 'Password and Confirm Password match';
     message.style.color = 'green';
   }
-} 
+}
 
 const signupFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = document.querySelector('#name-signup').value.trim();
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-    if (name && username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, username, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      const jsonedRes = await response.json();
-      alert(jsonedRes.message);
-      if (response.ok) {
-        document.location.replace('/login');
-      }
+  const username = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  if (username && email && password) {
+    const response = await fetch('/api/users/signup', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const jsonedRes = await response.json();
+    alert(jsonedRes.message);
+
+    if (response.ok) {
+      document.location.replace('/login');
     }
-  };
+  }
+};
 
 const signup = document.querySelector('.signup-form');
 signup.addEventListener('submit', signupFormHandler);
