ae978ba5438a0764a5e1b742cc7a6ae4eac4ca54
marissacraig <gersen.marissa@gmail.com>
2023-10-01 20:14:39 -0700

    styled account page

---
+++ public/js/settings.js
@@ -0,0 +1,76 @@
+async function editApplicationFormHandler(event) {
+    event.preventDefault();
+  
+    let title = document.getElementById('jobTitle').value.trim();
+    let company = document.getElementById('company').value.trim();
+    let status = document.getElementById('status').value.trim();
+    const location = document.getElementById('location').value.trim();
+    const url = document.getElementById('companyURL').value.trim();
+    const description = document.getElementById('description').value.trim();
+    const salary = document.getElementById('salary').value.trim();
+  
+    //get id of application
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    console.log(id);
+  
+    // Get custom text input values for fields that have "other" option selected
+    if (title == "Other") {
+      title = document.getElementById('otherJobTitle').value.trim();;
+    }
+    if (company == "Other") {
+      company = document.getElementById('otherCompany').value.trim();
+    }
+    if (status == "Other") {
+      status = document.getElementById('otherStatus').value.trim();
+    }
+  
+    if (title && company && status && location && url && description && salary) {
+      const response = await fetch(`/api/applications/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, company, status, location, url, description, salary }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  async function deleteAccountBtn(event) {
+    event.preventDefault();
+  
+    const confirm = confirm('Are you sure you want to delete your account?');
+        if (confirm) {
+    
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+    if (id) {
+      const response = await fetch(`/api/users/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+          id
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/login');
+      } else {
+        alert(response.statusText);
+      }
+    }}}
+  
+  document.querySelector('.edit-application-form')
+  .addEventListener('submit', editApplicationFormHandler);
+  const deleteAccount = document.querySelector('#delete-account');
+  deleteAccount.addEventListener('click', deleteAccountBtn);
